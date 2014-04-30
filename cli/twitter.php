2f369a40d5e6f8296d9e09f3b35463b21081d557
Tim Lytle <tim@timlytle.net>
2014-04-30 10:07:35 -0400

    Updated the error logging, now with stats.

--- cli/twitter.php
+++ cli/twitter.php
@@ -7,12 +7,16 @@ use GuzzleHttp\Stream as Stream;
 require_once '../vendor/autoload.php';
 $config = include '../config.php';
 
+error_log('loaded config');
+
 //http client
 $client = new HttpClient();
 
 //oauth setup
 $oauth = new Oauth1($config['oauth']);
 $client->getEmitter()->attach($oauth);
+
+error_log('setup oauth');
 
 //tracked keywords
 $track = [
@@ -31,16 +35,29 @@ $request = $client->createRequest(
 //set the track keywords
 $request->getBody()->setField('track', implode(',', $track));
 
+error_log('created stream request');
+
 //get the streamed response
 $response = $client->send($request);
 $stream = $response->getBody();
 
+error_log('connected to stream');
+
 //read lines from the response
+$count = 0;
+$start = $time = time();
 while(!$stream->eof()){
     $tweet = Stream\read_line($stream);
     $tweet = json_decode($tweet, true);
     if(isset($tweet['text'])){
-        error_log($tweet['text']);
+        $count++;
         file_put_contents('tweets.txt', $tweet['text'] . PHP_EOL, FILE_APPEND);
+    }
+
+    if(time() > ($time + 30)){
+        error_log('tweets collected: ' . $count);
+        $elapsed = time() - $start;
+        error_log('tweets / minute: ' . $count/($elapsed/60));
+        $time = time();
     }
 }
